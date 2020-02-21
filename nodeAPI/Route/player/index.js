e68b5b98124860c6cd313f08351e446e8f7c9476
hetviprajapati <hetviprajapati114@gmail.com>
2020-02-21 15:24:59 +0530

    forth

--- nodeAPI/Route/player/index.js
+++ nodeAPI/Route/player/index.js
@@ -7,6 +7,7 @@ var multer=require('multer');
 var bcrypt=require('bcryptjs');
 var jwt=require('../../Helper/jwtHelper.js');
 
+
 const storage=multer.diskStorage({
   destination:function(req,file,cb){
     cb(null,'./uploads')
